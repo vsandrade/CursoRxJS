6236a751a15becb220066274e6e4149ea60bc686
Vinicius Andrade <ozirispc@gmail.com>
2022-05-15 18:59:12 -0400

    Typescript - Initial Setup

---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+const path = require('path');
+
+module.exports = {
+    mode: process.env.NODE_ENV,
+    entry: {
+        // test: [ './src/test.ts' ],
+        index: [ './src/index.ts' ]
+    },
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                use: 'ts-loader',
+                exclude: /node_modules/,
+            },
+        ],
+    },
+    resolve: {
+        extensions: ['.tsx', '.ts', '.js'],
+    },
+    output: {
+        filename: '[name].js',
+        path: path.resolve(__dirname, 'public', 'dist'),
+    },
+};
