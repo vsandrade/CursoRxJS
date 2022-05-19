c88c2dcb61b567dd382670fa29da9692daf30a3c
Vinicius Andrade <ozirispc@gmail.com>
2022-05-18 22:06:52 -0400

    Objeto Criado

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,18 @@
-console.log('Vinicius Andrade');
+import { from } from "rxjs";
+
+let numbers = [1,5,10];
+let source = from(numbers);
+
+class MyObserver {
+    next(value: Object) {
+        console.log(value)
+    }
+    error(e: Error) {
+        console.log(`error: ${e}`)
+    }
+    complete() {
+        console.log('Completed');
+    }
+}
+
+source.subscribe(new MyObserver());
