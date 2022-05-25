79f6fee81e34a48deaa29bf9dc7ab6ad84eae5c0
Vinicius Andrade <ozirispc@gmail.com>
2022-05-25 14:21:39 -0400

    (Switch || Merge)Map

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,4 @@
-import { fromEvent } from 'rxjs';
-import { delay, map, filter } from 'rxjs/operators';
+import { switchMap, mergeMap, fromEvent, Observable } from 'rxjs';
 
 interface IMovie {
     title: string;
@@ -10,24 +9,34 @@ let button = document.getElementById('button');
 
 let click = fromEvent(button, 'click');
 
-function load(url: string) {
-    let xhr = new XMLHttpRequest();
+function load(url: string): Observable<any> {
+    return new Observable(subscriber => {
+        let xhr = new XMLHttpRequest();
 
-    xhr.addEventListener('load', () => {
-        let movies = JSON.parse(xhr.responseText);
-        movies.forEach((movie: IMovie) => {
-            let div = document.createElement("div");
-            div.innerText = movie.title;
-            output.appendChild(div);
+        xhr.addEventListener('load', () => {
+            let data = JSON.parse(xhr.responseText);
+            subscriber.next(data)
+            subscriber.complete();
         });
-    })
 
-    xhr.open('GET', url);
-    xhr.send();
+        xhr.open('GET', url);
+        xhr.send();
+    })
 }
 
-click.subscribe({
-    next: () => load('../movies.json'),
-    error: (e: Error) => console.log(`error: ${e}`),
-    complete: () => console.log('Completed'),
+function renderMovies(movies: IMovie[]) {
+    movies.forEach((movie: IMovie) => {
+        let div = document.createElement('div');
+        div.innerText = movie.title;
+        output.appendChild(div);
+    });
+}
+
+click.pipe(
+    mergeMap(e => load('../movies.json'))
+    //switchMap(e => load('../movies.json'))
+).subscribe({
+     next: renderMovies,
+     error: (e: Error) => console.log(`error: ${e}`),
+     complete: () => console.log('Completed'),
 });
